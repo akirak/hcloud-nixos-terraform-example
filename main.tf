7b2adcef0513e639958e3d66a28388da6f859d9b
Akira Komamura <akira.komamura@gmail.com>
2022-12-26 17:07:13 +0900

    style: Apply terraform fmt

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 resource "hcloud_ssh_key" "default" {
-    name = "default"
-    public_key = var.public_key
+  name       = "default"
+  public_key = var.public_key
 }
 
 resource "hcloud_server" "default" {
   name        = "default"
   image       = "debian-11"
   server_type = "cpx21"
-  location = "hil"
+  location    = "hil"
   public_net {
     ipv4_enabled = true
     ipv6_enabled = false
