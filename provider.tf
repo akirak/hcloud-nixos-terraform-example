9f9b9f447ea4e681f8957fbe14eb88caf90a214d
Akira Komamura <akira.komamura@gmail.com>
2022-12-26 17:01:07 +0900

    Add configuration for Terraform Cloud

--- provider.tf
+++ provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "1.36.2"
     }
   }
+
+  cloud {
+    organization = "akirak"
+    workspaces {
+      name = "github-actions-hetzner-cloud-example"
+    }
+  }
 }
 
 provider "hcloud" {
