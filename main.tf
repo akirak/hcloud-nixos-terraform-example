45681ccbb78ffa4aa5a55b05d39139d77ee7ea55
Akira Komamura <akira.komamura@gmail.com>
2022-12-28 01:28:00 +0900

    Bring my own config

--- main.tf
+++ main.tf
@@ -32,7 +32,8 @@ resource "hcloud_server" "default" {
 
   provisioner "remote-exec" {
     inline = [
-      "command -v nix || curl https://raw.githubusercontent.com/elitak/nixos-infect/master/nixos-infect | NIX_CHANNEL=nixos-22.11 NO_REBOOT=1 bash 2>&1 | tee /tmp/infect.log"
+      "command -v nix || curl https://raw.githubusercontent.com/elitak/nixos-infect/master/nixos-infect | NIX_CHANNEL=nixos-22.11 NO_REBOOT=1 bash 2>&1 | tee /tmp/infect.log",
+      "shutdown -r 1"
     ]
   }
 }
