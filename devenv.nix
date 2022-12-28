43e9623bff70aaeaf290dd03fabc11ac90948117
Akira Komamura <akira.komamura@gmail.com>
2022-12-28 17:54:02 +0900

    build: Add scripts to devenv.nix

--- devenv.nix
+++ devenv.nix
@@ -4,4 +4,17 @@
   # Terraform
   languages.terraform.enable = true;
   pre-commit.hooks.terraform-format.enable = true;
+
+  scripts.format.exec = ''
+    terraform fmt
+  '';
+
+  scripts.lint.exec = ''
+    terraform fmt -check -write=false
+  '';
+
+  scripts.validate.exec = ''
+    terraform validate
+    tfsec
+  '';
 }
