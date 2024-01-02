a7dee45535408870f1f328e542c33c44811eb5ea
Akira Komamura <akira.komamura@gmail.com>
2024-01-03 00:19:47 +0900

    Remove Terraform Cloud

--- provider.tf
+++ provider.tf
@@ -5,13 +5,6 @@ terraform {
       version = "1.36.2"
     }
   }
-
-  cloud {
-    organization = "akirak"
-    workspaces {
-      name = "github-actions-hetzner-cloud-example"
-    }
-  }
 }
 
 provider "hcloud" {
