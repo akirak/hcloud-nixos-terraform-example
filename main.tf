2d8ae8b4f232272756c3cf4910ccaff5a17dfc26
Akira Komamura <akira.komamura@gmail.com>
2024-01-03 01:31:55 +0900

    Revert "Create a private key in the same directory"
    This reverts commit c2c3d171f67a2019566002764eff067439dcf752.

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "hcloud_ssh_key" "ephemeral_ssh_key" {
 }
 
 resource "local_sensitive_file" "private_key" {
-  filename        = "id_ed25519"
+  filename        = "${path.module}/terraform-cloud.pem"
   content         = var.private_key
   file_permission = "0600"
 }
